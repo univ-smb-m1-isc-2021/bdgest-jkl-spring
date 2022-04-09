2f28d9eba451044cc81c77fae52a3b06eda47c28
LuxemTheFez <37304100+LuxemTheFez@users.noreply.github.com>
2022-04-09 19:02:49 +0200

    Import cors

--- src/main/java/com/manga/bdgest/controller/AuteurController.java
+++ src/main/java/com/manga/bdgest/controller/AuteurController.java
@@ -6,10 +6,7 @@ import com.manga.bdgest.service.AlbumService;
 import com.manga.bdgest.service.AuteurService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/manga/bdgest/controller/CompteController.java
+++ src/main/java/com/manga/bdgest/controller/CompteController.java
@@ -5,10 +5,7 @@ import com.manga.bdgest.model.Compte;
 import com.manga.bdgest.service.CompteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
