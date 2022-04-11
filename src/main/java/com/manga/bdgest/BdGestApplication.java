95b09b3fd81e6b0cfb95456fc1c538d3956d0df3
Omar <omarkhial74@gmail.com>
2022-04-11 17:05:14 +0200

    test about github actions

--- src/main/java/com/manga/bdgest/BdGestApplication.java
+++ src/main/java/com/manga/bdgest/BdGestApplication.java
@@ -1,7 +1,10 @@
 package com.manga.bdgest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class BdGestApplication {
